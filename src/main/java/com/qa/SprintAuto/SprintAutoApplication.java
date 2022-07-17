80d82c572be8ba06c2f463d8bb39432e8335f8de
Nilkanth11 <nilvadukar11@gmail.com>
2022-07-17 11:27:54 +0530

    init commit

---
+++ src/main/java/com/qa/SprintAuto/SprintAutoApplication.java
@@ -0,0 +1,13 @@
+package com.qa.SprintAuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintAutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/SprintAuto/SprintAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.SprintAuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
